8ff154a84a8bd5b93e9a1bab3a66b4052c899a6c
Abdulquadri <odeshinaquhadri@gmail.com>
2020-02-13 11:31:51 +0100

    Added new songs

--- music/views.py
+++ music/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Album
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    all_albums = Album.objects.all()
+    context = {'all_albums': all_albums, }
+    return render(request, 'music/index.html', context)
+
+
+def details(request, album_id):
+    album = Album.objects.get(pk=album_id)
+    context = {'album': album, }
+    return render(request, 'music/details.html', context)
