8bc6b2fb254db63d70be665370caa4384a66a249
Abdulquadri <odeshinaquhadri@gmail.com>
2020-02-18 14:58:08 +0100

    added favorive and unfavourite functionality

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+app_name = "music"
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<album_id>[0-9]+)/$', views.details, name='details'),
+    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),
+    url(r'^(?P<album_id>[0-9]+)/unfavourite/$', views.unfavourite, name='unfavourite'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from .models import Album
-
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
+from django.http import Http404
 # Create your views here.
 
 
@@ -9,8 +9,28 @@ def index(request):
     context = {'all_albums': all_albums, }
     return render(request, 'music/index.html', context)
 
-
 def details(request, album_id):
-    album = Album.objects.get(pk=album_id)
-    context = {'album': album, }
-    return render(request, 'music/details.html', context)
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/details.html', {"album": album, })
+
+def favourite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/details.html', {'album': album, 'error_message': "Cant just do that", })
+    else:
+        selected_song.is_favourite = True
+        selected_song.save()
+        return render(request, 'music/details.html', {"album": album, })
+
+def unfavourite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/details.html', {'album': album, 'error_message': "Cant just do that", })
+    else:
+        selected_song.is_favourite = False
+        selected_song.save()
+        return render(request, 'music/details.html', {"album": album, })
